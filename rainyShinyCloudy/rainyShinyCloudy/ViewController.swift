356eab6eb9c88ad72500bc2331ce9653dc70d979
Mostafa Oraei <oraei.mossi@gmail.com>
2017-07-15 11:52:43 +0430

    View template design completed and pods installed

---
+++ rainyShinyCloudy/rainyShinyCloudy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  rainyShinyCloudy
+//
+//  Created by Mostafa Oraei on 4/24/1396 AP.
+//  Copyright © 1396 Mostafa Oraei. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
