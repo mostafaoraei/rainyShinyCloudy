5ee4f864b9b538c98535dd3f2ec7382519d598cc
Mostafa Oraei <oraei.mossi@gmail.com>
2017-07-15 12:47:27 +0430

    Add tableView prototype and make some fancy change in appearance For deselection mode and delete insets in the left side

--- rainyShinyCloudy/rainyShinyCloudy/ViewController.swift
+++ rainyShinyCloudy/rainyShinyCloudy/ViewController.swift
@@ -8,18 +8,41 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var dateLbl: UILabel!
+    
+    @IBOutlet weak var currentTempLbl: UILabel!
+    
+    @IBOutlet weak var locationLbl: UILabel!
+    
+    @IBOutlet weak var weatherImageView: UIImageView!
+    
+    @IBOutlet weak var weatherTypeLbl: UILabel!
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
     }
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 6
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "weatherCell", for: indexPath)
+        
+        return cell
+    }
 
 }
 
